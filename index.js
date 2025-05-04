4e5ff5194cd6e9258841d1398211ce406c56eb78
Arnau-Mas <arnau.mava@gmail.com>
2025-05-04 16:03:32 +0200

    added express static dist

--- index.js
+++ index.js
@@ -3,6 +3,7 @@ const cors = require("cors");
 var morgan = require('morgan')
 
 const app = express()
+app.use(express.static('dist'))
 app.use(cors())
 app.use(express.json())
 app.use(morgan('tiny')) 
